d00e33573111545f6cd2321d6f235ef930762722
Valkyrux <philadephos.granata@gmail.com>
2021-11-30 11:00:41 +0100

    init

---
+++ js-jsnack-falsa-lista-invitati/js/script.js
@@ -0,0 +1,8 @@
+// liste nomi e cognomi
+userFirstNames = ["Fulvio", "Francesco", "Fabio", "Flavio", "Giandomenico", "Giovanni", "Salvatore", "Antonio", "Daniele", "Chiara", "Elisabetta", "Elisa", "Miriam", "Giovanna"];
+userLastNames = ["Barilla", "Ferrero", "De Cecco", "Vinciguerra", "Cognomeacaso", "Nessunidea", "Tantoperriempire", "Ebbastadai"];
+// funzione che calcola la combinazione di due array, estraendo elementi casuali
+function getFullNames (firstNames, lastNames, numberofFullnames) {
+    let listOfFullNames = [];
+    for (let i = 0; i < )
+}
